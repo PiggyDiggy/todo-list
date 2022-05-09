5b7d857d0053b551fca76aa9d637247c13809038
Bull <dmitry55580@gmail.com>
2022-05-09 23:23:18 +0300

    Local storage data saving

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@ export default {
   components: {
     Header,
   },
+  beforeCreate() {
+    this.$store.commit("loadFromLocalStorage");
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,57 +1,10 @@
 import { createStore } from "vuex";
 
-const saveTodos = (todos) => {
-  localStorage.setItem("todos", JSON.stringify(todos));
-};
-
-const loadTodos = () => {
-  const todos = localStorage.getItem("todos");
-  return todos ? JSON.parse(todos) : [];
-};
-
 const store = createStore({
   state() {
     return {
-      todos: [
-        {
-          title: "Completed",
-          memo: "Completed123",
-          important: false,
-          createdAt: new Date(2022, 4, 10, 10),
-          dateCompleted: new Date(2022, 4, 10, 12),
-          id: 1,
-        },
-        {
-          title: "Second",
-          memo: "Completed123",
-          important: false,
-          createdAt: new Date(2022, 4, 10, 10),
-          dateCompleted: new Date(2022, 4, 10, 11),
-          id: 2,
-        },
-        {
-          title: "Another One",
-          memo: "Sample",
-          important: true,
-          createdAt: new Date(2022, 4, 10, 9),
-          dateCompleted: new Date(2022, 4, 10, 15),
-          id: 3,
-        },
-        {
-          title: "Test",
-          memo: "Test123",
-          important: false,
-          createdAt: new Date(2022, 4, 10, 8),
-          id: 4,
-        },
-        {
-          title: "Second",
-          important: true,
-          createdAt: new Date(2022, 4, 10, 9),
-          id: 5,
-        },
-      ],
-      todoNextId: 6,
+      todos: [],
+      todoNextId: 0,
     };
   },
   mutations: {
@@ -71,11 +24,46 @@ const store = createStore({
       const todo = state.todos.find((todo) => todo.id === id);
       [todo.title, todo.memo] = [title, memo];
     },
+    loadFromLocalStorage(s) {
+      const state = loadTodos();
+      convertJSONToDate(state.todos);
+      this.replaceState(state);
+    },
   },
   getters: {
     completed: (state) => state.todos.filter((todo) => todo.dateCompleted),
     current: (state) => state.todos.filter((todo) => !todo.dateCompleted),
   },
+});
+
+const loadTodos = () => {
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+  const todoNextId = JSON.parse(localStorage.getItem("nextId")) || 0;
+  return { todos, todoNextId };
+};
+
+const convertJSONToDate = (todos) => {
+  todos.forEach((todo) => {
+    todo.createdAt = new Date(todo.createdAt);
+    if (todo.dateCompleted) {
+      todo.dateCompleted = new Date(todo.dateCompleted);
+    }
+  });
+};
+
+const saveTodos = (todos) => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const saveNextId = (id) => {
+  localStorage.setItem("nextId", JSON.stringify(id));
+};
+
+store.subscribe((mutation, state) => {
+  if (mutation.type === "loadFromLocalStorage")
+    return;
+  if (mutation.type === "addTodo") saveNextId(state.todoNextId);
+  saveTodos(state.todos);
 });
 
 export default store;
